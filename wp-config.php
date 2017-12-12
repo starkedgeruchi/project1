06c00a0307139ba5e5e4508dcf56ca3c0124585d
starkedgeruchi <34435147+starkedgeruchi@users.noreply.github.com>
2017-12-12 15:43:34 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'ahaapps');
+define('DB_NAME', 'ahaapps-test');
 
 /** MySQL database username */
 define('DB_USER', 'root');
